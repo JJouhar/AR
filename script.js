f15f33f320fee3443767e75f450164a24a15a4eb
JJouhar <153763509+JJouhar@users.noreply.github.com>
2024-01-05 17:08:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
+        position: '-0 0.1 -0.8',
         scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
@@ -52,7 +53,7 @@ var setModel = function (model, entity) {
     }
 
     if (model.position) {
-        entity.setAttribute('position', model.position);
+        //entity.setAttribute('position', model.position);
     }
 
     entity.setAttribute('gltf-model', model.url);
